ae223553bc7c5650aef651a9a25c89d1b03e1aea
Kevin Jackson <kj31428@gmail.com>
2019-04-13 21:50:54 -0700

    Adds Delivery class.

--- DriverTips/Model/Controller/DeliveriesViewController.swift
+++ DriverTips/Model/Controller/DeliveriesViewController.swift
@@ -12,7 +12,6 @@ class DeliveriesViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
---
+++ DriverTips/Model/Delivery.swift
@@ -0,0 +1,50 @@
+
+//
+//  File.swift
+//  DriverTips
+//
+//  Created by Kevin Jackson on 4/13/19.
+//  Copyright © 2019 Kevin Jackson. All rights reserved.
+//
+
+import Foundation
+
+class Delivery: NSObject {
+    
+    var address: String
+    var cash: Double
+    var credit: Double
+    var date: Date
+    var order: Int
+    var notes: String
+    var payout: Double
+    var tripComp: Double
+    
+    init(address: String = "", cash: Double = 0, credit: Double = 0, date: Date = Date(), order: Int = 0, notes: String = "", payout: Double = 0, tripComp: Double = 0) {
+        self.address = address
+        self.cash = cash
+        self.credit = credit
+        self.date = date
+        self.order = order
+        self.notes = notes
+        self.payout = payout
+        self.tripComp = tripComp
+    }
+    
+    convenience init(random: Bool = true) {
+        if random {
+            self.init(address: ["1908 Morgan", "200 S Catalina", "19430 Entradero"][Int.random(in: 0...2)],
+                      cash: Double.random(in: 0...10),
+                      credit: Double.random(in: 0...10),
+                      date: Date(),
+                      order: Int.random(in: 0...300),
+                      notes: ["", "Left pizza", "Scary dogs"][Int.random(in: 0...2)],
+                      tripComp: 1.75
+                      )
+        }
+        else {
+            self.init()
+        }
+    }
+
+}
